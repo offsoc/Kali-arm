0b2795f383fb7fb569e6d8418d88e1b926171af4
Ben Wilson <g0tmi1k@kali.org>
2024-03-24 13:32:02 +0000

    debug_enable: Switch to filename, rather than path
    ./$0 vs /path/to/$0

--- common.d/functions.sh
+++ common.d/functions.sh
@@ -80,7 +80,7 @@ EOF
 
 # Debug function
 function debug_enable() {
-    log="./logs/${0%.*}-$(date +"%Y-%m-%d-%H-%M").log"
+    log="./logs/$( basename ${a%%.*} )-$(date +"%Y-%m-%d-%H-%M").log"
     mkdir -p ./logs/
     log "Debug: Enabled" green
     log "Output: ${log}" green
